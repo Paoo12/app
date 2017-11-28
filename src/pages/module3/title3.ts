bb153240b5daa676bfb18f166e382b5c9bbd09f1
paoo <paolosalvador28@gmail.com>
2017-11-29 02:37:27 +0800

    12332
    11111

--- src/pages/module3/title3.ts
+++ src/pages/module3/title3.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { PopoverPage } from '../popover/popover';
 import { PopoverController } from 'ionic-angular/components/popover/popover-controller';
+import { P3Page } from '../module3/page3/page3';
 
 @Component({
   selector: 'page-title3',
@@ -18,5 +19,10 @@ export class Title3Page {
     popover.present({
       ev: myEvent
     });
+  }
+
+  start()
+  {
+    this.navCtrl.push(P3Page);
   }
 }
--- src/pages/module4/title4.ts
+++ src/pages/module4/title4.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { PopoverPage } from '../popover/popover';
 import { PopoverController } from 'ionic-angular/components/popover/popover-controller';
+import { P4Page } from '../module4/page4/page4';
 
 @Component({
   selector: 'page-title4',
@@ -19,4 +20,9 @@ export class Title4Page {
       ev: myEvent
     });
   }
+  start(){
+    this.navCtrl.push(P4Page);
+  }
+
+
 }
